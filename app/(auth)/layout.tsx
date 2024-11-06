c7e38ea21f2d9f66652317267520598154a12891
VictorAkubo <victorugbede89@gmail.com>
2024-11-06 13:51:51 -0800

    latest update

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -8,8 +8,8 @@ export default function RootLayout({
     return (
         <main className="flex min-h-screen w-full justify-around font-inter">
             {children}
-            <div className="auth-asset">
-                <div>
+            <div className="auth-asset -mt-[390px]">
+                <div className="-mt-[390px]">
                     <Image src="/icons/auth-image.svg" alt="Auth image" width={500} height={500} />
                 </div>
             </div>
